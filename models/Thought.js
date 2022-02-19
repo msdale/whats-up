276b7d9b164f0debb46bbbffa4eefd09713d90bd
Mark Dale <msdaledad@gmail.com>
2022-02-19 14:06:04 -0500

    intermediate testing

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,37 @@
 const { Schema, model } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
+const ReactionSchema = new Schema(
+  {
+    // set custom id to avoid confusion with parent thought _id
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 280
+    },
+    username: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
+
 const ThoughtSchema = new Schema(
   {
     thoughtText: {
